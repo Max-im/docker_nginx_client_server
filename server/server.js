da2d0a3ab4816b730456217334f5538b61084a08
Maksym Pozhydaiev <m.pozhydaiev@astoundcommerce.com>
2023-04-08 15:39:25 +0300

    remove redic

--- server/server.js
+++ server/server.js
@@ -1,11 +1,6 @@
 const os = require('os');
 const express = require('express');
 const app = express();
-const redis = require('redis');
-const redisClient = redis.createClient({
-  host: 'redis',
-  port: 6379
-});
 
 app.get('/', function(req, res) {
     res.json({hello: 'world'});
