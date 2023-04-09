bac34a9ddea3729186e914b129141af84456ddcf
Maksym Pozhydaiev <m.pozhydaiev@astoundcommerce.com>
2023-04-09 19:07:29 +0300

    rewrite compose

--- server/index.js
+++ server/index.js
@@ -1,22 +1,29 @@
 const {app} = require('./app');
-// const {sequelize} = require('./db');
-const { Pool } = require('pg');
+const {sequelize} = require('./db');
+// const { Pool } = require('pg');
 
 
 (async ()=> {
-    // await sequelize.authenticate();
-    const pgClient = new Pool({
-        user: process.env.DB_USER,
-        host: process.env.DB_HOST,
-        database: process.env.DB_NAME,
-        password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT,
-    });
+    sequelize.authenticate()
+    .then(() => {
+        app.listen(5000, () => console.log('app run'));
+    })
+    .catch(err) {
+        console.log('=====================')
+        console.log(err)
+        console.log('=====================')
+    }
+    // const pgClient = new Pool({
+    //     user: process.env.DB_USER,
+    //     host: process.env.DB_HOST,
+    //     database: process.env.DB_NAME,
+    //     password: process.env.DB_PASSWORD,
+    //     port: process.env.DB_PORT,
+    // });
 
-    pgClient
-    .query('CREATE TABLE IF NOT EXISTS aaa(number INT)')
-    .catch((err) => console.error('not create table: ' + err));
+    // pgClient
+    // .query('CREATE TABLE IF NOT EXISTS aaa(number INT)')
+    // .catch((err) => console.error('not create table: ' + err));
 
 
-    app.listen(5000, () => console.log('app run'));
 })()
