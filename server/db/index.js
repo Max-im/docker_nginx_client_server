dd26e663debaa7526a0968c80a7b185321ecf872
Maksym Pozhydaiev <m.pozhydaiev@astoundcommerce.com>
2023-04-09 18:47:13 +0300

    rewrite compose

--- server/db/index.js
+++ server/db/index.js
@@ -4,6 +4,10 @@ const dbName = process.env.DB_NAME;
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 
+console.log("===============================")
+console.log(dbName, dbUser, dbPassword)
+console.log("===============================")
+
 const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
 	dialect: 'postgres',
 	host: process.env.DB_HOST,
