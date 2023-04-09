c59c2dbc6472f62645ec9c7f837f08e3392386eb
Maksym Pozhydaiev <m.pozhydaiev@astoundcommerce.com>
2023-04-09 18:54:11 +0300

    rewrite compose

--- server/index.js
+++ server/index.js
@@ -1,7 +1,22 @@
 const {app} = require('./app');
-const {sequelize} = require('./db');
+// const {sequelize} = require('./db');
+const { Pool } = require('pg');
+
 
 (async ()=> {
-    await sequelize.authenticate();
+    // await sequelize.authenticate();
+    const pgClient = new Pool({
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
+    });
+
+    pgClient
+    .query('CREATE TABLE IF NOT EXISTS aaa(number INT)')
+    .catch((err) => console.error('not create table: ' + err));
+
+
     app.listen(5000, () => console.log('app run'));
 })()
