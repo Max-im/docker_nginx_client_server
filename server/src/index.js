e4e19c5e9c6d2ed218c62d1b3a37952fa70338e0
Maksym Pozhydaiev <m.pozhydaiev@astoundcommerce.com>
2023-04-14 19:16:51 +0300

    remove sequelize

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,6 @@
 const {app} = require('./app');
-const {sequelize} = require('./db');
 
 (async ()=> {
     const PORT = process.env.PORT || 5000; 
-    await sequelize.authenticate();
     app.listen(PORT, () => console.log(`server run on port ${PORT}`));
 })()
