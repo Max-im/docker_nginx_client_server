4c14652d8ab1c2df1e7927aaf3f712f4c573eadb
Maksym Pozhydaiev <m.pozhydaiev@astoundcommerce.com>
2023-04-14 14:11:30 +0300

    add config db

---
+++ server/src/config/db.js
@@ -0,0 +1,25 @@
+const config = require('./index');
+
+module.exports = {
+    development: {
+        username: config.dbUser,
+        password: config.dbPassword,
+        database: config.dbName,
+        host: config.dbHost,
+        dialect: "postgres"
+    },
+    test: {
+        username: config.dbUser,
+        password: config.dbPassword,
+        database: config.dbName,
+        host: config.dbHost,
+        dialect: "postgres"
+    },
+    production: {
+        username: config.dbUser,
+        password: config.dbPassword,
+        database: config.dbName,
+        host: config.dbHost,
+        dialect: "postgres"
+    }
+}
